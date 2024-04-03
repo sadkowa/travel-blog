99112b572951a34385c521b6b2321ad29520ee31
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-04-03 11:24:38 +0200

    Change categories section appearance

--- src/components/CategoriesSection/CategoriesSection.js
+++ src/components/CategoriesSection/CategoriesSection.js
@@ -10,11 +10,12 @@ const CategoriesSection = ({ categories, currentCategory = 'all', setPage }) => 
 
     const styles = {
         textDecoration: 'none',
-        color: 'black'
+        color: 'black',
+        backgroundColor: 'white'
     }
 
     const activeStyle = {
-        border: '1px solid #ccc'
+        backgroundColor: '#ccc'
     }
 
     return <StyledCategoriesSection>
@@ -39,15 +40,10 @@ export default CategoriesSection
 
 const StyledCategoryInSection = styled(StyledCategory)(
     () => (css`
-        padding: ${({theme}) => theme.spaces.xxsmall} ;
+        padding: ${({theme}) => theme.spaces.xxsmall};
         width: 100%;
         min-width: 50px;
         text-align: center;
-        background-color: white;
-
-        ${({ $active }) => $active && css`
-            border: 1px solid #ccc;
-        `}
 
         @media ${({theme}) => theme.media.tablet} {
             min-width: 85px;
